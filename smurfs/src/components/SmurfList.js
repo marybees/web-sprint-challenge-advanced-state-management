e45dead311a840e74d5ba02276c7eed0b445bc9b
Mary Baker <meb601@gmail.com>
2020-09-11 12:08:50 -0400

    Build out SmurfList

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "../index.css"
+import { connect } from "react-redux";
+import Smurf from "./Smurf";
+
+const SmurfList = props => {
+
+    return (
+        <div className="smurf-list">
+            <div>{smurfs.map((smurf) => {
+                return <Smurf key={smurf.id} name={smurf.name} age={smurf.age} height={smurf.height} smurf={smurf} />;
+            })}</div>
+        </div>
+    );
+
+}
+
+function mapStateToProps(state) {
+    return {
+        smurfs: state.smurfs
+    };
+}
+
+export default connect(mapStateToProps, {})(SmurfList);
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    rootElement
+);
